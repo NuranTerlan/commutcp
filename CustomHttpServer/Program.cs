a8a0708cddb188c0e3509de2f2044714cd0e374e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-16 16:03:39 +0400

    starting point of http listener windows service

---
+++ CustomHttpServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+
+namespace CustomHttpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Http-Listener service is started..\n");
+            var ports = ConfigurationManager.AppSettings.Get("listening-ports")?
+                .Trim().Split(';');
+            using (var listener = new RequestListener(ports))
+            {
+                listener.Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
