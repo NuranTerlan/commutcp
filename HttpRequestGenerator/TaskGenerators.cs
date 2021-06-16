80b3b0cfe1e61c6eaf25dab84424f0eb783a87db
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-06-16 16:06:22 +0400

    task generators extension class is implemented

---
+++ HttpRequestGenerator/TaskGenerators.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HttpRequestGenerator
+{
+    public static class TaskGenerators
+    {
+        public static IEnumerable<Task> GenerateNHttpClient(int n, int reqsPerClient)
+        {
+            var returnedTasks = new List<Task>();
+            var clientHelper = new ClientHelper(n);
+            
+            for (var i = 0; i < n; i++)
+            {
+                returnedTasks.Add(Task.Run(() => clientHelper
+                    .SendNPostRequestsParallel(reqsPerClient, "Message from Client# " + Guid.NewGuid())));
+            }
+
+            return returnedTasks;
+        }
+    }
+}
